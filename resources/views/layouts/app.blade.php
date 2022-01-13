3715f87677a2f82068666795a1d896febd91bb77
Agujas1993 <samuel.smr2.local@gmail.com>
2022-01-13 13:47:34 +0100

    Slider página principal, intentado hacer lo de dusk y tests de subcategorías, pendiente de ello

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,12 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('vendor/glider-js/glider.min.css') }}">
         @livewireStyles
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ asset('vendor/glider-js/glider.min.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
@@ -52,5 +54,6 @@
                 }
             }
         </script>
+        @stack('scripts')
     </body>
 </html>
--- tests/Browser/SubcategoryTest.php
+++ tests/Browser/SubcategoryTest.php
@@ -6,8 +6,10 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTest extends DuskTestCase
+class SubcategoryTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function testBasicExample()
     {
@@ -15,7 +17,7 @@ class ExampleTest extends DuskTestCase
             $browser->visit('/')
                 ->click('Categorías')
                 ->mouseover('Celulares y tablets')
-                    ->assertSee('Celulares y Smartphones');
+                ->assertSee('Celulares y Smartphones');
         });
     }
 }
