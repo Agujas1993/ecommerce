5f11ae21bd5af02995a05928b0aff8e501c8c151
Agujas1993 <samuel.smr2.local@gmail.com>
2022-01-21 20:24:46 +0100

    Test 9 realizado a falta de mejorar

--- tests/Browser/ProductsTest.php
+++ tests/Browser/ProductsTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Browser;
 
+use App\Http\Livewire\AddCartItemColor;
+use App\Http\Livewire\AddCartItemSize;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
+use Livewire\Livewire;
 use Tests\DuskTestCase;
 
 class ProductsTest extends DuskTestCase
@@ -83,5 +86,37 @@ class ProductsTest extends DuskTestCase
                 ->assertUrlIs('http://localhost:8000/products/' . $product3->id)
                 ->screenshot('productDetailsAccess3-test');
         });*/
+    }
+
+    /** @test */
+    public function the_color_and_size_dropdowns_are_shown_according_to_the_chosen_product()
+    {
+        $colorCategory = Category::first();
+        $product = $colorCategory->products()->first();
+
+        $this->get('products/' . $product->id)
+            ->assertSeeLivewire('add-cart-item-color');
+
+        $this->browse(function (Browser $browser) use($product) {
+            $browser->visit('products/' . $product->id)
+                ->assertSee('Color:')
+                ->assertPresent('select.form-control')
+                ->screenshot('colorDropdown-test');
+        });
+
+        $sizeColorCategory = Category::skip(4)->first();
+        $sizeProduct = $sizeColorCategory->products()->first();
+
+        $this->get('products/' . $sizeProduct->id)
+            ->assertSeeLivewire('add-cart-item-size');
+
+        $this->browse(function (Browser $browser) use($sizeProduct) {
+            $browser->visit('products/' . $sizeProduct->id)
+                ->assertSee('Color:')
+                ->assertSee('Talla:')
+                ->assertPresent('div > select')
+                ->assertPresent('div.mt-2 > select')
+                ->screenshot('colorSizeDropdown-test');
+        });
     }
 }
