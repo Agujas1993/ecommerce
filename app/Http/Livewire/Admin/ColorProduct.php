befd14ed25de14e2422c2fa1b070380e4e0bfff8
Agujas1993 <samuel.smr2.local@gmail.com>
2022-01-28 10:23:03 +0100

    A medio paso componente color-product página 6, cambiar todos los tests, nunca se coge nada del modelo en los tests, siempre se crea, hay que cambiar la base de datos a la de test, replantear qué tests hacer, a veces es mejor dusk y otras phpunit.

--- app/Http/Livewire/Admin/ColorProduct.php
+++ app/Http/Livewire/Admin/ColorProduct.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Color;
 use Livewire\Component;
 
 class ColorProduct extends Component
 {
+    protected $rules = [
+        'color_id' => 'required',
+        'quantity' => 'required|numeric'
+    ];
+
+    public $product, $colors;
+    public $color_id, $quantity;
+    public $open = false;
+
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
     public function render()
     {
-        return view('livewire.admin.color-product');
+        $productColors = $this->product->colors;
+
+        return view('livewire.admin.color-product', compact('productColors'));
+    }
+
+    public function edit()
+    {
+        $this->open = true;
+    }
+
+    public function save(){
+        $this->validate();
+        $this->product->colors()->attach([
+            $this->color_id => [
+                'quantity' => $this->quantity
+            ]
+        ]);
+        $this->reset(['color_id', 'quantity']);
+        $this->emit('saved');
+        $this->product = $this->product->fresh();
     }
 }
