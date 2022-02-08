7707985671d786c14273e354d77c8cfc5f0888ea
Agujas1993 <samuel.smr2.local@gmail.com>
2022-02-08 17:42:37 +0100

    Todos los tests de la semana 4 realizados menos el 6, 8, 9, hay que refactorizar todos los tests de dusk, hacer que funcionen los mismos en phpunit y refactorizarlos también, test 11 semana 3 hay que refactorizarle código y mejorar el 5 de la semana 4, hay que pasar todos los tests de dusk a phpunit según ha dicho Carlos, hay que crear componentes para input, radio, select y hacer test semana 4, hacer tareas de la semana 5

--- tests/Feature/BdTest.php
+++ tests/Feature/BdTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\Subcategory;
 use App\Models\User;
@@ -82,6 +83,78 @@ class BdTest extends TestCase
         $this->post('/login', $credentials);
 
         $this->get('/shopping-cart')->assertSee($product->name);
+
+    }
+
+    /** @test */
+    public function the_stock_changes_when_creating_an_order(){
+        $user = User::factory()->create([
+            'name' => 'Samuel Garcia',
+            'email' => 'samuel@test.com',
+            'password' => bcrypt('123'),
+        ]);
+
+        $category = Category::factory()->create(["name" => "Celulares y tablets",
+            "slug" => "celulares-y-tablets",
+            "icon" => 'algo',
+            "image" => "categories/84b8093bb4bc5ec8f29c8edc374caf22.png",
+        ]);
+
+        $subcategory = Subcategory::create([
+                'category_id' => 1,'name' => 'Celulares y smartphones',
+                'slug' => Str::slug('Celulares y smartphones'),
+            ]
+        );
+
+        $brand = $category->brands()->create(['name' => 'LG']);
+
+        $product = Product::factory()->create([
+            'name' => 'Tablet LG2080',
+            'slug' => Str::slug('Tablet LG2080'),
+            'description' => 'Tablet LG2080' . 'moderno año 2022',
+            'subcategory_id' => $subcategory->id,
+            'brand_id' => $brand->id,
+            'price' => '118.99',
+            'quantity' => '20',
+            'status' => 2
+        ]);
+
+        $this->get('/login')->assertSee('Correo electrónico');
+        $credentials = [
+            "email" => $user->email,
+            "password" => '123'
+        ];
+
+        $response = $this->post('/login', $credentials);
+        $response->assertRedirect('/dashboard');
+        $this->assertCredentials($credentials);
+
+        Cart::add([
+            'id' => $product->id,
+            'name' => $product->name,
+            'qty' => '2',
+            'price' => $product->price,
+            'weight' => 550,
+        ]);
+
+        $order = new Order();
+        $order->user_id = '1';
+        $order->contact = 'Samuel';
+        $order->phone = '42343423234';
+        $order->envio_type = '2';
+        $order->shipping_cost = 0;
+        $order->total = '40';
+        $order->content = Cart::content();
+        $order->status = 2;
+        $order->save();
+
+        json_decode($order->content);
+        foreach (Cart::content() as $item) {
+            discount($item);
+        }
+
+        $this->assertDatabaseHas('products', ['id' => $product->id, 'name' => $product->name, 'quantity' => $product->quantity - 2]);
+
 
     }
 }
