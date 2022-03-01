56a6d2a4e2f3e841c9ee59f7ae729535df49f6b7
Agujas1993 <samuel.smr2.local@gmail.com>
2022-03-01 21:16:43 +0100

    Paso 2 semana 5 vistas mejoradas, falta poder filtrar por todo el stock, también ordenar por color, talla y stock, hay que actualizar a Laravel 9 en otra rama a poder ser para ver si no explota

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <style>
+            [x-cloak] { display: none; }
+        </style>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
